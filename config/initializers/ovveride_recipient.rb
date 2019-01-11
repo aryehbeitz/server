b05ae7b3e26fd27c4982394a28d9984196baa5e0
Omer Kahani <kahaniomer@gmail.com>
2019-01-11 16:42:36 +0200

    type

--- config/initializers/ovveride_recipient.rb
+++ config/initializers/ovveride_recipient.rb
@@ -1,6 +1,6 @@
 class OverrideRecipient
   def self.delivering_email(mail)
-    mail.to = ENV["SENDGRID_USERNAME"]
+    mail.to = ENV["OVVERIDE_RECIPIENT"]
   end
 end
 ActionMailer::Base.register_interceptor(OverrideRecipient)
